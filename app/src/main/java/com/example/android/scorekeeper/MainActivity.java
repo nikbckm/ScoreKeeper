907cdee1a88de37b3e19271341b6c1e7e347c004
Niklas Böckmann <33151122+nikbckm@users.noreply.github.com>
2018-02-20 20:05:27 +0100

    udacity changes

--- app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -18,13 +18,39 @@ public class MainActivity extends AppCompatActivity {
         score_textView = findViewById(R.id.score_textView);
     }
 
-    public void fieldgoal_TeamA(View view) { ScoreTeamA = ScoreTeamA+3; score_textView.setText(""+ScoreTeamA+" : "+ScoreTeamB);}
-    public void fieldgoal_TeamB(View view) { ScoreTeamB = ScoreTeamB+3; score_textView.setText(""+ScoreTeamA+" : "+ScoreTeamB);}
-    public void touchdown_TeamA(View view) { ScoreTeamA = ScoreTeamA+7; score_textView.setText(""+ScoreTeamA+" : "+ScoreTeamB);}
-    public void touchdown_TeamB(View view) { ScoreTeamB = ScoreTeamB+7; score_textView.setText(""+ScoreTeamA+" : "+ScoreTeamB);}
-    public void twopoint_TeamA(View view) { ScoreTeamA = ScoreTeamA+2; score_textView.setText(""+ScoreTeamA+" : "+ScoreTeamB);}
-    public void twopoint_TeamB(View view) { ScoreTeamB = ScoreTeamB+2; score_textView.setText(""+ScoreTeamA+" : "+ScoreTeamB);}
-    public void extrapoint_TeamA(View view) { ScoreTeamA++; score_textView.setText(""+ScoreTeamA+" : "+ScoreTeamB);}
-    public void extrapoint_TeamB(View view) { ScoreTeamB++;  score_textView.setText(""+ScoreTeamA+" : "+ScoreTeamB);}
-    public void resetScore(View view) { ScoreTeamA=0; ScoreTeamB=0; score_textView.setText(""+ScoreTeamA+" : "+ScoreTeamB);}
+    public void fieldgoal_TeamA(View view) {
+        ScoreTeamA = ScoreTeamA+3; score_textView.setText(""+ScoreTeamA+" : "+ScoreTeamB);
+    }
+
+    public void fieldgoal_TeamB(View view) {
+        ScoreTeamB = ScoreTeamB+3; score_textView.setText(""+ScoreTeamA+" : "+ScoreTeamB);
+    }
+
+    public void touchdown_TeamA(View view) {
+        ScoreTeamA = ScoreTeamA+7; score_textView.setText(""+ScoreTeamA+" : "+ScoreTeamB);
+    }
+
+    public void touchdown_TeamB(View view) {
+        ScoreTeamB = ScoreTeamB+7; score_textView.setText(""+ScoreTeamA+" : "+ScoreTeamB);
+    }
+
+    public void twopoint_TeamA(View view) {
+        ScoreTeamA = ScoreTeamA+2; score_textView.setText(""+ScoreTeamA+" : "+ScoreTeamB);
+    }
+
+    public void twopoint_TeamB(View view) {
+        ScoreTeamB = ScoreTeamB+2; score_textView.setText(""+ScoreTeamA+" : "+ScoreTeamB);
+    }
+
+    public void extrapoint_TeamA(View view) {
+        ScoreTeamA++; score_textView.setText(""+ScoreTeamA+" : "+ScoreTeamB);
+    }
+
+    public void extrapoint_TeamB(View view) {
+        ScoreTeamB++;  score_textView.setText(""+ScoreTeamA+" : "+ScoreTeamB);
+    }
+
+    public void resetScore(View view) {
+        ScoreTeamA=0; ScoreTeamB=0; score_textView.setText(""+ScoreTeamA+" : "+ScoreTeamB);
+    }
 }
